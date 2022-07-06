025422dec34becb73e196abed38f98dfba5519cd
a-cole55 <arniqua_c@yahoo.com>
2022-07-06 11:15:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import logo from "./assets/default-monochrome.svg";
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav>
+        <div class="dropdown">
+            <button class="dropbtn">
+                <div class="container">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                </div>
+            </button>
+            <div class="dropdown-content">
+              <a id="aboutMe" href='#about'>About</a>
+              <a id="menuSelection" href='#menu'>Menu</a>
+              <a id="Placeorder" href='#order'>Order</a>
+              <a id="contactUs" href='#contact'>Contact</a>
+            </div>
+          </div>
+      <img src={logo} alt="logo" />
+      <ShoppingBagIcon className="shop" fontSize="large"/>
+    </nav>
       </header>
     </div>
   );
