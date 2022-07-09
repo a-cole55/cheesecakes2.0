a4339afc4602457b950697e82ec63b45c9b445b3
a-cole55 <arniqua_c@yahoo.com>
2022-07-09 15:44:52 -0500

    added quantity count

--- src/components/Cheesecake.js
+++ src/components/Cheesecake.js
@@ -1,16 +1,26 @@
-
+import { useState } from "react";
 
 export default function Cheesecake(props){
+    const [quantity, setQuantity] = useState(0);
+
+    function addItem(){
+        setQuantity(quantity < 10 ? quantity + 1: quantity)
+    }
+
+    function removeItem(){
+        setQuantity(quantity > 0 ? quantity - 1 : 0)
+    }
+
     return(
         <div className="cheesecake">
                 <img src={props.img} alt=""></img>
                 <h3>{props.title}</h3>
                 <span id="price">{props.price}</span>
-                <button id="order">Add to Order</button>
+                <button id="order" onClick={quantity}>Add to Order</button>
                 <div id="quantity">
-                    <button id="remove">-</button>
-                    <input disabled id="qtyCount" placeholder="0"></input>
-                    <button id="add">+</button>
+                    <button id="remove" onClick={removeItem}>-</button>
+                    <input disabled id="qtyCount" value={quantity}></input>
+                    <button id="add" onClick={addItem}>+</button>
                 </div>
         </div>
     )
