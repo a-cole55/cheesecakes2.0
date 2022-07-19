e0bdacea15b50d8d3a83ceb73b29b59f395d8468
a-cole55 <arniqua_c@yahoo.com>
2022-07-18 21:46:35 -0500

    upload owner image

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,10 @@
-import "../assets/owner.jpg"
+import Owner from "../assets/owner.jpg"
 export default function About(){
     return(
         <div>
             <h1>Meet the Baker💫</h1>
             <h2>Chelsea Jones</h2>
-            <img src="../assets/owner.jpg" alt="owner"></img>
+            <img src={Owner} alt="owner"></img>
             <p><strong>Welcome to Cheesecakes by Chelsea!</strong><br></br>My name is Chelsea Jones. Originally from Kansas and now an Austin resident. I love being creative in the kitchen and experimenting with new cheesecake flavors. 
             I am so happy that you have chosen to place an order with me! I look forward to providing you with the best quality cheesecake you have ever experienced!</p>
 
