630a5ef01f2db84434a245a18b797d80b0cba3aa
a-cole55 <arniqua_c@yahoo.com>
2022-07-21 18:15:32 -0500

    home page animation fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,7 @@ export default function Home(){
             <img id="homeLogo" src={HomeLogo} alt="logo"></img>
             <motion.p
             initial={{ y: "-50%"}}
-            animate={{ y: 0, transition: {delay: 0.2, ...transition}}}
+            animate={{ y: 0, transition: {delay: 0.4, ...transition}}}
             exit={{ opacity: 0}}
             transition={transition}>Welcome Cheesecake Lover! I look forward to providing you with the best quality cheesecakes you have ever experienced.</motion.p>
         </motion.div>
