610215d09b0cf677d9618992548cb29ae9cd7715
a-cole55 <arniqua_c@yahoo.com>
2022-08-05 14:41:09 -0500

    updated wording to shopping page

--- src/components/EmptyCart.js
+++ src/components/EmptyCart.js
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 export default function EmptyCart(){
     return (
         <div className="cartContent2">
-            <h2><Link to="/menu"><strong id="visitHereLink">Visit Here</strong></Link> To Add Items to Your Shopping Cart</h2>
+            <h2><Link to="/menu"><strong id="visitHereLink">Visit Here</strong></Link> To Add Items to Your Cart</h2>
             <img id="smallImg" className="hide" src={Home2} alt="cheesecake img"></img>
             <img id="bgImg" className="hide" src={HomeImg} alt="cheesecake img"></img>
             
