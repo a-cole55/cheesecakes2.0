ebd6d5fd2cbc5f274cb6b25c97dad291db4635b9
a-cole55 <arniqua_c@yahoo.com>
2022-08-10 16:18:21 -0500

    header styling updated and made fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,7 +30,7 @@ export default function Header(props) {
               </ul>
               <div className="actions">
                 <Link to="/order"><ShoppingCartOutlinedIcon className='shop' id="hideNavLogo" fontSize='large'/>
-                <span id="cartHeading">Cart </span></Link>
+                <span id="cartHeading">Cart</span></Link>
                 {/* conditional statement if over 9 make 9+ & if equal to 0 don't display*/}
                 {quantity > 0 && 
                 <span id="cartQty">{quantity > 9 ? "(9+)" : `(${quantity})`}</span>}
